46bb9de49759001242faef9b069cb3911eb660b4
gwthmsb <gowtham.sb1390@gmail.com>
2021-03-22 14:14:51 +0530

    [LDA] LDA as feature reducer

--- LDA.py
+++ LDA.py
@@ -5,12 +5,17 @@ from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+"""
+    LinearDiscriminantAnalysis is classifier not an reducer. 
+"""
+
 
 def check_model(X, y, model):
     cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
     scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
     print(scores)
     print("Mean accuracy: %.3f, SD: %.3f"%(mean(scores), std(scores)))
+
 
 def simple_prediction(X, y, model):
     model.fit(X, y)
@@ -19,6 +24,7 @@ def simple_prediction(X, y, model):
     yhat = model.predict([row])
     # summarize prediction
     print('Predicted Class: %d' % yhat)
+
 
 def hyper_parameters(X, y, model, solver):
     """
@@ -29,28 +35,62 @@ def hyper_parameters(X, y, model, solver):
     grid = {"solver": [solver,]}
     search_model = GridSearchCV(model, grid, scoring='accuracy', cv=cv, n_jobs=-1)
     results = search_model.fit(X, y)
-    return results
     print("Solver: {}".format(results.best_params_))
     print("Accuracy: %.8f"%results.best_score_)
+    return results
 
 
-def main():
+def iris_data():
     # define dataset
     #X, y = make_classification(n_samples=1000, n_features=10, n_informative=10, n_redundant=0, random_state=1)
     X, y = load_iris(True)
     print(X, y)
     # define model
     model = LinearDiscriminantAnalysis()
+    print(model.get_params(True))
     #simple_prediction(X, y, model)
     solvers = ["svd", "lsqr", "eigen"]
     row = [0.1, 3.5, 4.2, 100]
     for solver in solvers:
         result = hyper_parameters(X, y, model, solver)
-        pr_class = result.predict([10,25, 30])
+        pr_class = result.predict([[10, 25, 30, 40]])
         print(pr_class)
 
 
+def iris_data_reduction():
+    X, y = load_iris(True)
+    model = LinearDiscriminantAnalysis(n_components=2)
+    model.fit(X, y)
+    X_trans = model.transform(X)
+    print(X_trans)
+    model.fit(X_trans, y)
+    print(model.predict([[0, 0]]))
+    print(model.predict([[100, 100]]))
+    print(model.predict([[-100, -100]]))
+
+
+def prediction():
+    X, y = make_classification(n_samples=1000, n_features=10, n_informative=10, n_redundant=0, random_state=1)
+    model = LinearDiscriminantAnalysis()
+    simple_prediction(X, y, model)
+
+
+def LDA_as_reduction():
+    X, y = make_classification(n_samples=10, n_features=6, n_informative=6, n_redundant=0, random_state=1)
+    model = LinearDiscriminantAnalysis(n_components=1)
+    print(X, y)
+    print(model.get_params(True))
+    model.fit(X, y)
+    print(model.predict([[2, 4, 5, -1, 0, 4]]))
+    X_trans = model.transform(X)
+    print(X_trans)
+    model.fit(X_trans, y)
+    print(model.predict([[2]]))
+    print(model.predict([[-3]]))
 
 
 if __name__ == "__main__":
-    main()
+    #prediction()
+    #iris_data()
+    iris_data_reduction()
+    #LDA_as_reduction()
